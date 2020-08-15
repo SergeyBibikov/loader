956456ce55636d056620b8c8d01757dac3394c2d
Sergey <sergey.bibikov.92@mail.ru>
2020-08-15 10:05:04 +0300

    added threads generation

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,21 @@ struct Request {
 
 
 fn main() {
-    init().unwrap();
+    let req_d = init().unwrap();    
+    let request_threads:Vec<std::thread::JoinHandle<()>>;
+    if req_d.protocol=="http"&&req_d.method=="GET"{
+        request_threads = gen_http_get_reqs(&req_d);
+    }
+    else if req_d.protocol=="https"&&req_d.method=="GET"{
+        request_threads = gen_https_get_reqs(&req_d);
+    }
+    else if req_d.protocol=="http"&&req_d.method=="POST"{
+        request_threads = gen_http_post_reqs(&req_d);
+    }
+    else {
+        request_threads = gen_https_post_reqs(&req_d);
+    }
+    for j in request_threads{ j.join().unwrap(); } 
 }
 
 fn init() -> Result<Request>{
@@ -46,4 +60,71 @@ fn init() -> Result<Request>{
              req.path_to_body.trim());
 
     Ok(req)
+}
+fn gen_http_get_reqs(input: &Request) -> Vec<std::thread::JoinHandle<()>>{
+    let mut request_threads = vec![];
+    let reqs_p_conn = input.max_reqs_per_conn;
+    for _ in 0..input.thread_num{
+        let path = input.path.clone();
+        let domain = input.domain.clone();
+        let port = input.port.clone();
+        let headers = input.headers.clone();
+        //let body = req_body.clone();
+        request_threads.push(std::thread::spawn(move ||{
+            get_req(&path,&domain,&port,&headers,&reqs_p_conn);
+            //tls_post_req(&path,&domain,&port,&body,&headers);                
+        }));
+    }
+    request_threads
+}
+
+fn gen_https_get_reqs(input: &Request) -> Vec<std::thread::JoinHandle<()>>{
+    let mut request_threads = vec![];
+    let reqs_p_conn = input.max_reqs_per_conn;
+    for _ in 0..input.thread_num{
+        let path = input.path.clone();
+        let domain = input.domain.clone();
+        let port = input.port.clone();
+        let headers = input.headers.clone();
+        request_threads.push(std::thread::spawn(move ||{
+            tls_get_req(&path,&domain,&port,&headers,&reqs_p_conn);                
+        }));
+    }
+    request_threads
+}
+
+fn gen_http_post_reqs(input: &Request) -> Vec<std::thread::JoinHandle<()>>{
+    let mut request_threads = vec![];
+    let reqs_p_conn = input.max_reqs_per_conn;
+    let temp_req_body = fs::read(&input.path_to_body).unwrap();
+    let req_body = String::from_utf8(temp_req_body).unwrap();
+    for _ in 0..input.thread_num{
+        let path = input.path.clone();
+        let domain = input.domain.clone();
+        let port = input.port.clone();
+        let headers = input.headers.clone();
+        let body = req_body.clone();
+        request_threads.push(std::thread::spawn(move ||{
+            post_req(&path,&domain,&port,&body,&headers,&reqs_p_conn);               
+        }));
+    }
+    request_threads
+}
+
+fn gen_https_post_reqs(input: &Request) -> Vec<std::thread::JoinHandle<()>>{
+    let mut request_threads = vec![];
+    let reqs_p_conn = input.max_reqs_per_conn;
+    let temp_req_body = fs::read(&input.path_to_body).unwrap();
+    let req_body = String::from_utf8(temp_req_body).unwrap();
+    for _ in 0..input.thread_num{
+        let path = input.path.clone();
+        let domain = input.domain.clone();
+        let port = input.port.clone();
+        let headers = input.headers.clone();
+        let body = req_body.clone();
+        request_threads.push(std::thread::spawn(move ||{
+            tls_post_req(&path,&domain,&port,&body,&headers,&reqs_p_conn);               
+        }));
+    }
+    request_threads
 }
